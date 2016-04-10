86aa8840e55a4fd4798a7f72b0a7da23e50d3904
WenyiY <chrisyin@udel.edu>
2016-04-10 16:49:19 -0400

    test of recycleGame(not started)

--- model/screenItems/RecycleGameScreenItems/AppleCore.java
+++ model/screenItems/RecycleGameScreenItems/AppleCore.java
@@ -3,8 +3,6 @@ package screenItems.RecycleGameScreenItems;
 
 public class AppleCore extends Trash {
 	
-	
-
 	public AppleCore(double xPos, double yPos) {
 		super(xPos, yPos);
 		// TODO Auto-generated constructor stub
---
+++ tests/RecycleGameScreenItem/RecycleGameTestSI.java
@@ -0,0 +1,46 @@
+package RecycleGameScreenItem;
+
+import junit.framework.TestCase;
+/**
+ * test all the subclasses of RecycleGameScreenItem
+ * @author wenyiyin
+ *
+ */
+public class RecycleGameTestSI extends TestCase{
+	public void testAppleCore(){
+		
+	}
+	public void testArrow(){
+		
+	}
+	public void testBananaPeel(){
+		
+	}
+	public void testChipBag(){
+		
+	}
+	public void testDiscardItems(){
+		
+	}
+	public void testGlassBottle(){
+		
+	}
+	public void testNewspaper(){
+		
+	}
+	public void testTinCan(){
+		
+	}
+	public void testTrashBin(){
+		
+	}
+	public void testRecyclable(){
+		
+	}
+	public void testTrash(){
+		
+	}
+	public void testRecycleBin(){
+		
+	}
+}
